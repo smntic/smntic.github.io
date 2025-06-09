d89dea04077817e34cc5b78a042debee45afc32b
Simon Ashton <simonashton.dev@gmail.com>
2025-06-09 11:22:32 -0700

    Prepare for the overhaul

--- src/components/windows/WelcomeWindow.tsx
+++ src/components/windows/WelcomeWindow.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Window from '../Window';
+import warningIcon from '../../assets/icons/warning.webp'
 
 interface WelcomeWindowProps {
     onClose: () => void;
@@ -22,20 +23,16 @@ const WelcomeWindow: React.FC<WelcomeWindowProps> = ({ onClose, onClick, onProje
     const article = [8, 11, 18, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89].includes(integerYears) ? 'an' : 'a'; // assuming i don't live for >799 years...
 
     return (
-        <Window title="Welcome!" windowWidth={600} windowHeight={400} onClose={onClose} onClick={onClick} zIndex={zIndex} focused={focused}>
-            <p>
-                Hello! I am Simon Ashton, {article} {gregorianYears}-year-old
-                {tropicalYears !== gregorianYears && ` (${tropicalYears} in tropical years)`} programmer living in Canada.
-                Please feel free to look around my website and check out some of my projects!
-            </p>
-            <ul className="nav-list">
-                <li>
-                    <span className="nav-item link" onClick={onProjectsClick}>Projects</span>
-                </li>
-                <li>
-                    <span className="nav-item link" onClick={onContactClick}>Contact</span>
-                </li>
-            </ul>
+        <Window title="In progress..." windowWidth={570} windowHeight={150} onClose={onClose} onClick={onClick} zIndex={zIndex} focused={focused}>
+            <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                <img src={warningIcon} width={70} style={{ margin: "5px" }} />
+                <p>
+                  overhaul coming soon...
+                  i will be back.
+                  <br />
+                  maybe play a game of minesweeper while you wait?
+                </p>
+            </div>
         </Window>
     );
 }
