a7ab828c96688cc2cb2dbf14ee3fc983c6f459ec
Simon Ashton <simonashton.dev@gmail.com>
2025-08-15 08:30:04 -0700

    Fix image configuration for static deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import smnticIcon from 'images/theme/favicon.png';
 
 export default function Header() {
   const navButtonClasses = 'px-3 py-1 hover:bg-panel-light rounded transition !text-foreground';
@@ -9,7 +8,7 @@ export default function Header() {
     <header className="sticky top-0 w-full bg-panel-dark panel-shadow z-100">
       <div className="flex justify-between items-center max-w-5xl mx-auto p-2">
         <Link href="/" className={`${navButtonClasses} inline-flex items-center space-x-2`}>
-          <Image src={smnticIcon} alt="smntic icon" className="w-6 h-6" />
+          <Image src="/images/theme/favicon.png" alt="smntic icon" className="w-6 h-6" />
           <span className="font-semibold">smntic.dev</span>
         </Link>
         <nav className="flex space-x">
